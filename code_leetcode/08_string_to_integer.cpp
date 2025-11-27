4282462f4e4a2a29fe8a4b346383fd1b9d6d4098
zacherywaung <2691205573@qq.com>
2025-11-27 15:49:12 -0800

    add file leetcode08

---
+++ code_leetcode/08_string_to_integer.cpp
@@ -0,0 +1,70 @@
+//08 String to Interger(atoi)
+
+// Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer.
+
+// The algorithm for myAtoi(string s) is as follows:
+
+// Whitespace: Ignore any leading whitespace (" ").
+// Signedness: Determine the sign by checking if the next character is '-' or '+', assuming positivity if neither present.
+// Conversion: Read the integer by skipping leading zeros until a non-digit character is encountered or the end of the string is reached. If no digits were read, then the result is 0.
+// Rounding: If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then round the integer to remain in the range. Specifically, integers less than -231 should be rounded to -231, and integers greater than 231 - 1 should be rounded to 231 - 1.
+// Return the integer as the final result.
+
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int myAtoi(string s) {
+        string ret;
+        int flag = 1;
+        int ret_int = 0;
+        string::iterator it = s.begin();
+        while(*it == ' ')
+        {
+            it++;
+        }
+        if(*it == '-' || *it == '+')
+        {
+            if(*it == '-')
+                flag = -1;
+            it++;
+        }
+        while(*it == '0')
+        {
+            it++;
+        }
+        while((*it >= '0' && *it <= '9')&& it != s.end())
+        {
+            ret += *it;
+            it++;
+        }
+        if(ret.size() == 0)
+            return 0;
+        string::iterator rit = ret.begin();
+        while(rit != ret.end())
+        {
+            if(flag == 1)
+            {
+                if(ret_int > INT_MAX / 10 ||
+                (ret_int == INT_MAX / 10 && (*rit - '0') >= 7))
+                    return INT_MAX;
+            }
+            else if(ret_int > INT_MAX / 10 ||
+                (ret_int == INT_MAX / 10 && (*rit - '0') >= 8))
+                return INT_MIN;
+            ret_int *= 10;
+            ret_int += (*rit - '0');
+            rit++;
+        }
+        return ret_int * flag;
+    }
+};
+
+int main()
+{
+    Solution s;
+    string x("2147483646");
+    s.myAtoi(x);
+
+}
