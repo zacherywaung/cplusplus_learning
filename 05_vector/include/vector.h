5295c17cad45c13786ceb6c40176f1d2a2953a83
zacherywaung <2691205573@qq.com>
2025-12-06 23:43:48 -0800

    renew file 05_vector

---
+++ 05_vector/include/vector.h
@@ -0,0 +1,233 @@
+#include<iostream>
+#include<assert.h>
+using namespace std;
+
+namespace zw
+{
+  template<class T>
+  class vector
+  {
+  public:
+    // iterator is a pointer
+    typedef T* iterator;
+    typedef const T* const_iterator;
+
+    iterator begin()
+    {
+        return _start;
+    }
+
+    iterator end()
+    {
+        return _finish;
+    }
+
+    const_iterator cbegin()const
+    {
+        return _start;
+    }
+
+    const_iterator cend()const
+    {
+        return _finish;
+    }
+
+
+    // construct and destroy
+
+    vector()
+        :_start(nullptr)
+        ,_finish(nullptr)
+        ,_end_of_storage(nullptr)
+    {}
+
+    vector(int n, const T& value = T())
+    {
+        assert(n > 0);
+        _start = new T[n];
+        for(int i = 0; i < n; i++)
+        {
+            _start[i] = value;
+        }
+        _finish = _end_of_storage = _start + n;
+    }
+
+    template<class InputIterator>
+    vector(InputIterator first, InputIterator last)
+    {
+        size_t n = 0;
+        for(InputIterator it = first; it != last; it++)
+        {
+            n++;
+        }
+        _start = new T[n];
+        _finish = _end_of_storage = _start + n;
+        T* dest = _start;
+        while(dest != _finish)
+        {
+            *dest = *first;
+            ++dest;
+            ++first;
+        }
+    }
+
+    vector(const vector<T>& v)
+    {
+        vector<T> tmp(v._start, v._finish);
+        swap(tmp);
+    }
+
+    vector<T>& operator= (vector<T> v)
+    {
+        vector<T> tmp(v._start, v._finish);
+        swap(tmp);
+        return *this;
+    }
+
+    ~vector()
+    {
+        delete[] _start;
+        _start = _finish = _end_of_storage = nullptr;
+    }
+
+    // capacity
+
+    size_t size() const
+    {
+        return _finish - _start;
+    }
+
+    size_t capacity() const
+    {
+        return _end_of_storage - _start;
+    }
+
+    void reserve(size_t n)
+    {
+        if(n > capacity())
+        {
+            T* tmp = new T[n];
+            size_t old_size = size();
+            for(size_t i = 0; i < n; i++)
+            {
+                tmp[i] = _start[i];
+            }
+            delete[] _start;
+            _start = tmp;
+            _finish = tmp + old_size;
+            _end_of_storage = tmp + n;
+        }
+    }
+
+    void resize(size_t n, const T& value = T())
+    {
+        if(n <= _finish - _start)
+        {
+            _finish = _start + n;
+        }
+        else{
+            if(n > size())
+            {
+                reserve(n);
+            }
+            while(_finish != _start + n)
+            {
+                *_finish = value;
+                ++_finish;
+            }
+        }
+    }
+
+
+
+    ///////////////access///////////////////////////////
+
+    T& operator[](size_t pos)
+    {
+        return _start[pos];
+    }
+
+    const T& operator[](size_t pos)const
+    {
+        return _start[pos];
+    }
+
+    ///////////////modify/////////////////////////////
+
+    void push_back(const T& x)
+    {
+        if(_finish == _end_of_storage)
+        {
+            reserve(capacity() == 4 ? 0 : 2*capacity());
+        }
+        *_finish = x;
+        _finish++;
+    }
+
+    void pop_back()
+    {
+        assert(!empty());
+        _finish--;
+    }
+
+    bool empty()
+    {
+        return _start == _finish;
+    }
+
+    void swap(vector<T>& v)
+    {
+        std::swap(_start, v._start);
+        std::swap(_finish, v._finish);
+        std::swap(_end_of_storage, v._end_of_storage);
+    }
+
+    iterator insert(iterator pos, const T& x)
+    {
+        if(_finish == _end_of_storage)
+        {
+            size_t len = pos - _start;
+            reserve(capacity() == 0 ? 4 : 2*capacity());
+            pos = _start + len;
+        }
+        T* end = _finish - 1;
+        for(end = _finish - 1; end >= pos; end--)
+        {
+            *(end + 1) = *end;
+        }
+        *pos = x;
+        ++_finish;
+        return pos;
+    }
+
+    iterator erase(iterator pos)
+    {
+        assert(pos < _finish);
+        T* key = pos;
+        while(key + 1 < _finish)
+        {
+            *key = *(key + 1);
+            key++;
+        }
+        --_finish;
+        return pos;
+    }
+
+    void print_vector()
+    {
+        for(size_t i = 0; i < size(); i++)
+        {
+            cout << _start[i] << " ";
+        }
+        cout << endl;
+    }
+
+  private:
+
+    iterator _start = nullptr;
+
+    iterator _finish = nullptr;
+
+    iterator _end_of_storage = nullptr;
+  };
+}
--- 05_vector/src/test.cpp
+++ 05_vector/src/test.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include"../include/vector.h"
 using namespace std;
 
 void Test1()
@@ -29,10 +30,81 @@ void Test1()
         cout << e << ",";
     }
     cout << endl;
+    vector<int> v2(10);
+    for(auto& e : v2)
+    {
+        cout << e << ",";
+    }
+    cout << endl;
+}
+
+void Test2()
+{
+    zw::vector<int> v1;
+    zw::vector<int> v2(10,1);
+    string s1("hello world");
+    zw::vector<char> v3(s1.begin() + 2, s1.end());
+    zw::vector<char> v4(v3);
+}
+
+void Test3()
+{
+    zw::vector<int> v1(10,1);
+    cout << v1.size() << ":" << v1.capacity() << endl;
+    v1.reserve(15);
+    cout << v1.size() << ":" << v1.capacity() << endl;
+    v1.resize(5);
+    cout << v1.size() << ":" << v1.capacity() << endl;
+    v1.resize(20,2);
+    cout << v1.size() << ":" << v1.capacity() << endl;
+    zw::vector<int>::iterator it = v1.begin();
+    while(it != v1.end())
+    {
+        cout << *it << " ";
+        it++;
+    }
+    cout << endl;
+    for(auto i : v1)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+void Test4()
+{
+    zw::vector<int> v1(10,1);
+    v1[5] = 10;
+    for(size_t i = 0; i < 10; i++)
+    {
+        cout << v1[i] << " ";
+    }
+    cout << endl;
+}
+
+void Test5()
+{
+    string s1("hello world");
+    zw::vector<char> v1(s1.begin(), s1.end());
+    v1.push_back('&');
+    v1.print_vector();
+    v1.push_back('*');
+    v1.print_vector();
+    v1.pop_back();
+    v1.print_vector();
+    v1.insert(v1.begin() + 5, '&');
+    v1.print_vector();
+    v1.erase(v1.begin());
+    v1.print_vector();
+
 }
 
 int main()
 {
     Test1();
+    //Test2();
+    //Test3();
+    //Test4();
+    //Test5();
     return 0;
 }
