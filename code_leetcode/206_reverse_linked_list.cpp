6e05a1aada27a36a281cf48b56a84255dcc6abc3
zacherywaung <2691205573@qq.com>
2026-01-03 22:53:39 -0800

    add leetcode 206

---
+++ code_leetcode/206_reverse_linked_list.cpp
@@ -0,0 +1,48 @@
+//leetcode206 Reverse Linked List
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+// Example 1:
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+
+// Example 2:
+// Input: head = [1,2]
+// Output: [2,1]
+
+// Example 3:
+// Input: head = []
+// Output: []
+
+
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head){
+        return dfs(head);
+    }
+
+    ListNode* dfs(ListNode* head)
+    {
+        if(head == nullptr)
+        {
+            return nullptr;
+        }
+        if(head->next == nullptr)
+        {
+            return head;
+        }
+        ListNode* ret = dfs(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return ret;
+    }
+};
